509b72a3f7215ac6ed826bf91dab09a6f257bf11
himanshuc11 <himanshuchhatpar@gmail.com>
2025-05-10 16:06:31 +0530

    Implement parameter validation and add secure database constants for client authentication

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const QUERY_PARAMS = {
+    CLIENT_ID: 'clientId',
+    SESSION_TOKEN: 'sessionToken',
+    IV: 'iv'
+}
---
+++ db/index.ts
@@ -0,0 +1,11 @@
+interface SecureDbEntry {
+    clientSecret: string;
+    allowedOrigin: string;
+}
+
+export const SECURE_DB_DATA: { [key: string]: SecureDbEntry } = {
+    '4fa7fa681e31c2bf513bb838a691d533d219fc53957e75d1ff422fcfe6dcb057': {
+        clientSecret: 'ab5bd52e844eb1ec74f422cb494b449a38524c30e5ab00d6156d76a91094348e',
+        allowedOrigin: "localhost"
+    }
+}
