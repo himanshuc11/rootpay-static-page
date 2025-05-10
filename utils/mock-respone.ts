3e2ebecf0bc0aa69abd20acf1761d409cdf2cf04
himanshuc11 <himanshuchhatpar@gmail.com>
2025-05-11 01:05:53 +0530

    Update allowedOrigins in SECURE_DB_DATA and reduce mock response delay in createMockResponse

--- utils/mock-respone.ts
+++ utils/mock-respone.ts
@@ -4,7 +4,7 @@ export async function createMockResponse(action: keyof typeof MOCK_RESPONSES) {
     switch (action) {
       case MOCK_RESPONSES.SUCCESS:
         return new Promise((resolve) => 
-          setTimeout(() => resolve(MOCK_RESPONSES.SUCCESS), 1000)
+          setTimeout(() => resolve(MOCK_RESPONSES.SUCCESS), 100)
         );
       case MOCK_RESPONSES.FAILURE:
         return new Promise((_, reject) => 
